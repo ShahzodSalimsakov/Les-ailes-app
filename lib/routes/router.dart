386096a0a6a70480b6dc78a4b3e8422c819ae5d4
shahaa <projects.salim95@gmail.com>
2022-03-28 17:55:23 +0500

    order

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,4 +1,4 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:les_ailes/home_page.dart';
 import 'package:les_ailes/pages/aboutUs.dart';
 import 'package:les_ailes/pages/changeLang.dart';
@@ -7,6 +7,7 @@ import 'package:les_ailes/pages/franchise.dart';
 import 'package:les_ailes/pages/my_addresses.dart';
 import 'package:les_ailes/pages/my_orders.dart';
 import 'package:les_ailes/pages/notifications.dart';
+import 'package:les_ailes/pages/order_detail.dart';
 import 'package:les_ailes/pages/pickup.dart';
 import 'package:les_ailes/pages/privacy.dart';
 import 'package:les_ailes/pages/settings.dart';
@@ -16,30 +17,33 @@ import 'package:les_ailes/pages/terms.dart';
 import '../pages/profile.dart';
 
 @MaterialAutoRouter(replaceInRouteName: 'Page,Route', routes: [
+  AutoRoute(path: '/', name: 'HomePage', page: HomePage),
+  AutoRoute(path: 'signIn', name: 'SignInPage', page: SignInPage),
   AutoRoute(
-    path: '/',
-    name: 'HomePage',
-    page: HomePage,
-  ),
-  AutoRoute(path: '/signIn', name: 'SignInPage', page: SignInPage),
-  AutoRoute(
-    path: '/settings',
+    path: 'settings',
     name: 'SettingsPage',
     page: SettingsPage,
   ),
-  AutoRoute(path: '/about', name: 'AboutUsPage', page: AboutUsPage),
+  AutoRoute(path: 'about', name: 'AboutUsPage', page: AboutUsPage),
   AutoRoute(
-      path: '/notifications',
+      path: 'notifications',
       name: 'NotificationsPage',
       page: NotificationsPage),
-  AutoRoute(path: '/changeLang', name: 'changeLang', page: ChangeLang),
-  AutoRoute(path: '/privacy', name: 'PrivacyPolicy', page: Privacy),
-  AutoRoute(path: '/terms', name: 'Termsofuse', page: Terms),
-  AutoRoute(path: '/franchise', name: 'Franchise', page: Franchise),
-  AutoRoute(path: '/delivery', name: 'Delivery', page: DeliveryPage),
-  AutoRoute(path: '/pickup', name: 'Pickup', page: PickupPage),
-  AutoRoute(path: '/profile', name: 'Profile', page: ProfilePage),
-  AutoRoute(path: '/my_orders', name: 'MyOrders', page: MyOrders),
-  AutoRoute(path: '/my_addresses', name: 'MyAddresses', page: MyAddresses)
+  AutoRoute(path: 'changeLang', name: 'changeLang', page: ChangeLang),
+  AutoRoute(path: 'privacy', name: 'PrivacyPolicy', page: Privacy),
+  AutoRoute(path: 'terms', name: 'Termsofuse', page: Terms),
+  AutoRoute(path: 'franchise', name: 'Franchise', page: Franchise),
+  AutoRoute(path: 'delivery', name: 'Delivery', page: DeliveryPage),
+  AutoRoute(path: 'pickup', name: 'Pickup', page: PickupPage),
+  AutoRoute(path: 'profile', name: 'Profile', page: ProfilePage),
+  AutoRoute(
+      path: 'my_orders',
+      name: 'Myorders',
+      page: EmptyRouterPage,
+      children: [
+        AutoRoute(path: '', page: MyOrders),
+        AutoRoute(path: ':orderId', page: OrderDetail)
+      ]),
+  AutoRoute(path: 'my_addresses', name: 'MyAddresses', page: MyAddresses)
 ])
 class $AppRouter {}
--- lib/routes/router.gr.dart
+++ lib/routes/router.gr.dart
@@ -10,18 +10,19 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i15;
-import 'package:flutter/material.dart' as _i16;
+import 'package:auto_route/auto_route.dart' as _i13;
+import 'package:flutter/material.dart' as _i17;
 
 import '../home_page.dart' as _i1;
-import '../models/yandex_geo_data.dart' as _i17;
+import '../models/yandex_geo_data.dart' as _i18;
 import '../pages/aboutUs.dart' as _i4;
 import '../pages/changeLang.dart' as _i6;
 import '../pages/delivery.dart' as _i10;
 import '../pages/franchise.dart' as _i9;
 import '../pages/my_addresses.dart' as _i14;
-import '../pages/my_orders.dart' as _i13;
+import '../pages/my_orders.dart' as _i15;
 import '../pages/notifications.dart' as _i5;
+import '../pages/order_detail.dart' as _i16;
 import '../pages/pickup.dart' as _i11;
 import '../pages/privacy.dart' as _i7;
 import '../pages/profile.dart' as _i12;
@@ -29,99 +30,116 @@ import '../pages/settings.dart' as _i3;
 import '../pages/signIn.dart' as _i2;
 import '../pages/terms.dart' as _i8;
 
-class AppRouter extends _i15.RootStackRouter {
-  AppRouter([_i16.GlobalKey<_i16.NavigatorState>? navigatorKey])
+class AppRouter extends _i13.RootStackRouter {
+  AppRouter([_i17.GlobalKey<_i17.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i15.PageFactory> pagesMap = {
+  final Map<String, _i13.PageFactory> pagesMap = {
     HomePage.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.HomePage());
     },
     SignInPage.name: (routeData) {
       final args = routeData.argsAs<SignInPageArgs>(
           orElse: () => const SignInPageArgs());
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: _i2.SignInPage(key: args.key));
     },
     SettingsPage.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i3.SettingsPage());
     },
     AboutUsPage.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i4.AboutUsPage());
     },
     NotificationsPage.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i5.NotificationsPage());
     },
     ChangeLang.name: (routeData) {
       final args = routeData.argsAs<ChangeLangArgs>(
           orElse: () => const ChangeLangArgs());
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: _i6.ChangeLang(key: args.key));
     },
     PrivacyPolicy.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i7.Privacy());
     },
     Termsofuse.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i8.Terms());
     },
     Franchise.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i9.Franchise());
     },
     Delivery.name: (routeData) {
       final args =
           routeData.argsAs<DeliveryArgs>(orElse: () => const DeliveryArgs());
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData,
           child: _i10.DeliveryPage(key: args.key, geoData: args.geoData));
     },
     Pickup.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: _i11.PickupPage());
     },
     Profile.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i12.ProfilePage());
     },
-    MyOrders.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i13.MyOrders());
+    Myorders.name: (routeData) {
+      return _i13.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i13.EmptyRouterPage());
     },
     MyAddresses.name: (routeData) {
-      return _i15.MaterialPageX<dynamic>(
+      return _i13.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i14.MyAddresses());
+    },
+    MyOrders.name: (routeData) {
+      return _i13.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i15.MyOrders());
+    },
+    OrderDetail.name: (routeData) {
+      final pathParams = routeData.inheritedPathParams;
+      final args = routeData.argsAs<OrderDetailArgs>(
+          orElse: () =>
+              OrderDetailArgs(orderId: pathParams.getString('orderId')));
+      return _i13.MaterialPageX<dynamic>(
+          routeData: routeData,
+          child: _i16.OrderDetail(key: args.key, orderId: args.orderId));
     }
   };
 
   @override
-  List<_i15.RouteConfig> get routes => [
-        _i15.RouteConfig(HomePage.name, path: '/'),
-        _i15.RouteConfig(SignInPage.name, path: '/signIn'),
-        _i15.RouteConfig(SettingsPage.name, path: '/settings'),
-        _i15.RouteConfig(AboutUsPage.name, path: '/about'),
-        _i15.RouteConfig(NotificationsPage.name, path: '/notifications'),
-        _i15.RouteConfig(ChangeLang.name, path: '/changeLang'),
-        _i15.RouteConfig(PrivacyPolicy.name, path: '/privacy'),
-        _i15.RouteConfig(Termsofuse.name, path: '/terms'),
-        _i15.RouteConfig(Franchise.name, path: '/franchise'),
-        _i15.RouteConfig(Delivery.name, path: '/delivery'),
-        _i15.RouteConfig(Pickup.name, path: '/pickup'),
-        _i15.RouteConfig(Profile.name, path: '/profile'),
-        _i15.RouteConfig(MyOrders.name, path: '/my_orders'),
-        _i15.RouteConfig(MyAddresses.name, path: '/my_addresses')
+  List<_i13.RouteConfig> get routes => [
+        _i13.RouteConfig(HomePage.name, path: '/'),
+        _i13.RouteConfig(SignInPage.name, path: 'signIn'),
+        _i13.RouteConfig(SettingsPage.name, path: 'settings'),
+        _i13.RouteConfig(AboutUsPage.name, path: 'about'),
+        _i13.RouteConfig(NotificationsPage.name, path: 'notifications'),
+        _i13.RouteConfig(ChangeLang.name, path: 'changeLang'),
+        _i13.RouteConfig(PrivacyPolicy.name, path: 'privacy'),
+        _i13.RouteConfig(Termsofuse.name, path: 'terms'),
+        _i13.RouteConfig(Franchise.name, path: 'franchise'),
+        _i13.RouteConfig(Delivery.name, path: 'delivery'),
+        _i13.RouteConfig(Pickup.name, path: 'pickup'),
+        _i13.RouteConfig(Profile.name, path: 'profile'),
+        _i13.RouteConfig(Myorders.name, path: 'my_orders', children: [
+          _i13.RouteConfig(MyOrders.name, path: '', parent: Myorders.name),
+          _i13.RouteConfig(OrderDetail.name,
+              path: ':orderId', parent: Myorders.name)
+        ]),
+        _i13.RouteConfig(MyAddresses.name, path: 'my_addresses')
       ];
 }
 
 /// generated route for
 /// [_i1.HomePage]
-class HomePage extends _i15.PageRouteInfo<void> {
+class HomePage extends _i13.PageRouteInfo<void> {
   const HomePage() : super(HomePage.name, path: '/');
 
   static const String name = 'HomePage';
@@ -129,9 +147,9 @@ class HomePage extends _i15.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.SignInPage]
-class SignInPage extends _i15.PageRouteInfo<SignInPageArgs> {
-  SignInPage({_i16.Key? key})
-      : super(SignInPage.name, path: '/signIn', args: SignInPageArgs(key: key));
+class SignInPage extends _i13.PageRouteInfo<SignInPageArgs> {
+  SignInPage({_i17.Key? key})
+      : super(SignInPage.name, path: 'signIn', args: SignInPageArgs(key: key));
 
   static const String name = 'SignInPage';
 }
@@ -139,7 +157,7 @@ class SignInPage extends _i15.PageRouteInfo<SignInPageArgs> {
 class SignInPageArgs {
   const SignInPageArgs({this.key});
 
-  final _i16.Key? key;
+  final _i17.Key? key;
 
   @override
   String toString() {
@@ -149,35 +167,35 @@ class SignInPageArgs {
 
 /// generated route for
 /// [_i3.SettingsPage]
-class SettingsPage extends _i15.PageRouteInfo<void> {
-  const SettingsPage() : super(SettingsPage.name, path: '/settings');
+class SettingsPage extends _i13.PageRouteInfo<void> {
+  const SettingsPage() : super(SettingsPage.name, path: 'settings');
 
   static const String name = 'SettingsPage';
 }
 
 /// generated route for
 /// [_i4.AboutUsPage]
-class AboutUsPage extends _i15.PageRouteInfo<void> {
-  const AboutUsPage() : super(AboutUsPage.name, path: '/about');
+class AboutUsPage extends _i13.PageRouteInfo<void> {
+  const AboutUsPage() : super(AboutUsPage.name, path: 'about');
 
   static const String name = 'AboutUsPage';
 }
 
 /// generated route for
 /// [_i5.NotificationsPage]
-class NotificationsPage extends _i15.PageRouteInfo<void> {
+class NotificationsPage extends _i13.PageRouteInfo<void> {
   const NotificationsPage()
-      : super(NotificationsPage.name, path: '/notifications');
+      : super(NotificationsPage.name, path: 'notifications');
 
   static const String name = 'NotificationsPage';
 }
 
 /// generated route for
 /// [_i6.ChangeLang]
-class ChangeLang extends _i15.PageRouteInfo<ChangeLangArgs> {
-  ChangeLang({_i16.Key? key})
+class ChangeLang extends _i13.PageRouteInfo<ChangeLangArgs> {
+  ChangeLang({_i17.Key? key})
       : super(ChangeLang.name,
-            path: '/changeLang', args: ChangeLangArgs(key: key));
+            path: 'changeLang', args: ChangeLangArgs(key: key));
 
   static const String name = 'ChangeLang';
 }
@@ -185,7 +203,7 @@ class ChangeLang extends _i15.PageRouteInfo<ChangeLangArgs> {
 class ChangeLangArgs {
   const ChangeLangArgs({this.key});
 
-  final _i16.Key? key;
+  final _i17.Key? key;
 
   @override
   String toString() {
@@ -195,34 +213,34 @@ class ChangeLangArgs {
 
 /// generated route for
 /// [_i7.Privacy]
-class PrivacyPolicy extends _i15.PageRouteInfo<void> {
-  const PrivacyPolicy() : super(PrivacyPolicy.name, path: '/privacy');
+class PrivacyPolicy extends _i13.PageRouteInfo<void> {
+  const PrivacyPolicy() : super(PrivacyPolicy.name, path: 'privacy');
 
   static const String name = 'PrivacyPolicy';
 }
 
 /// generated route for
 /// [_i8.Terms]
-class Termsofuse extends _i15.PageRouteInfo<void> {
-  const Termsofuse() : super(Termsofuse.name, path: '/terms');
+class Termsofuse extends _i13.PageRouteInfo<void> {
+  const Termsofuse() : super(Termsofuse.name, path: 'terms');
 
   static const String name = 'Termsofuse';
 }
 
 /// generated route for
 /// [_i9.Franchise]
-class Franchise extends _i15.PageRouteInfo<void> {
-  const Franchise() : super(Franchise.name, path: '/franchise');
+class Franchise extends _i13.PageRouteInfo<void> {
+  const Franchise() : super(Franchise.name, path: 'franchise');
 
   static const String name = 'Franchise';
 }
 
 /// generated route for
 /// [_i10.DeliveryPage]
-class Delivery extends _i15.PageRouteInfo<DeliveryArgs> {
-  Delivery({_i16.Key? key, _i17.YandexGeoData? geoData})
+class Delivery extends _i13.PageRouteInfo<DeliveryArgs> {
+  Delivery({_i17.Key? key, _i18.YandexGeoData? geoData})
       : super(Delivery.name,
-            path: '/delivery', args: DeliveryArgs(key: key, geoData: geoData));
+            path: 'delivery', args: DeliveryArgs(key: key, geoData: geoData));
 
   static const String name = 'Delivery';
 }
@@ -230,9 +248,9 @@ class Delivery extends _i15.PageRouteInfo<DeliveryArgs> {
 class DeliveryArgs {
   const DeliveryArgs({this.key, this.geoData});
 
-  final _i16.Key? key;
+  final _i17.Key? key;
 
-  final _i17.YandexGeoData? geoData;
+  final _i18.YandexGeoData? geoData;
 
   @override
   String toString() {
@@ -242,32 +260,66 @@ class DeliveryArgs {
 
 /// generated route for
 /// [_i11.PickupPage]
-class Pickup extends _i15.PageRouteInfo<void> {
-  const Pickup() : super(Pickup.name, path: '/pickup');
+class Pickup extends _i13.PageRouteInfo<void> {
+  const Pickup() : super(Pickup.name, path: 'pickup');
 
   static const String name = 'Pickup';
 }
 
 /// generated route for
 /// [_i12.ProfilePage]
-class Profile extends _i15.PageRouteInfo<void> {
-  const Profile() : super(Profile.name, path: '/profile');
+class Profile extends _i13.PageRouteInfo<void> {
+  const Profile() : super(Profile.name, path: 'profile');
 
   static const String name = 'Profile';
 }
 
 /// generated route for
-/// [_i13.MyOrders]
-class MyOrders extends _i15.PageRouteInfo<void> {
-  const MyOrders() : super(MyOrders.name, path: '/my_orders');
+/// [_i13.EmptyRouterPage]
+class Myorders extends _i13.PageRouteInfo<void> {
+  const Myorders({List<_i13.PageRouteInfo>? children})
+      : super(Myorders.name, path: 'my_orders', initialChildren: children);
+
+  static const String name = 'Myorders';
+}
+
+/// generated route for
+/// [_i14.MyAddresses]
+class MyAddresses extends _i13.PageRouteInfo<void> {
+  const MyAddresses() : super(MyAddresses.name, path: 'my_addresses');
+
+  static const String name = 'MyAddresses';
+}
+
+/// generated route for
+/// [_i15.MyOrders]
+class MyOrders extends _i13.PageRouteInfo<void> {
+  const MyOrders() : super(MyOrders.name, path: '');
 
   static const String name = 'MyOrders';
 }
 
 /// generated route for
-/// [_i14.MyAddresses]
-class MyAddresses extends _i15.PageRouteInfo<void> {
-  const MyAddresses() : super(MyAddresses.name, path: '/my_addresses');
+/// [_i16.OrderDetail]
+class OrderDetail extends _i13.PageRouteInfo<OrderDetailArgs> {
+  OrderDetail({_i17.Key? key, required String orderId})
+      : super(OrderDetail.name,
+            path: ':orderId',
+            args: OrderDetailArgs(key: key, orderId: orderId),
+            rawPathParams: {'orderId': orderId});
 
-  static const String name = 'MyAddresses';
+  static const String name = 'OrderDetail';
+}
+
+class OrderDetailArgs {
+  const OrderDetailArgs({this.key, required this.orderId});
+
+  final _i17.Key? key;
+
+  final String orderId;
+
+  @override
+  String toString() {
+    return 'OrderDetailArgs{key: $key, orderId: $orderId}';
+  }
 }
