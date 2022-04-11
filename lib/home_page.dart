92a1773ced0579d736894d9d6dd8b79925896215
shahaa <projects.salim95@gmail.com>
2022-04-11 16:40:55 +0500

    statusbar color

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:les_ailes/utils/colors.dart';
 import 'package:les_ailes/widgets/ChooseCity.dart';
 import 'package:les_ailes/widgets/header.dart';
 import 'package:les_ailes/widgets/leftMenu.dart';
@@ -28,7 +29,7 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         systemOverlayStyle: const SystemUiOverlayStyle(
           systemNavigationBarColor: Colors.transparent, // Navigation bar
-          statusBarColor: Colors.transparent, // Status bar
+          statusBarColor: AppColors.mainColor, // Status bar
         ),
       ),
       drawer: const LeftMenu(),
